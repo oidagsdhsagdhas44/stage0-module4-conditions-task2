a2b8e12d59e5fc5147dfd47d7ab029363922207a
oidagsdhsagdhas44 <tornike.buchukuri@gau.edu.ge>
2023-02-21 00:34:29 +0400

    module4 task2

--- src/main/java/school/mjc/stage0/conditions/task2/CoinFlip.java
+++ src/main/java/school/mjc/stage0/conditions/task2/CoinFlip.java
@@ -1,7 +1,10 @@
 package school.mjc.stage0.conditions.task2;
 
 public class CoinFlip {
+//    Implement the program which will consume 1 number in method arguments and if the number is less than 500
+//    print "Eagle" otherwise "Tail" (range of numbers is [1-1000], no checks for that required):
     public void throwCoin(int from1UpTo1000) {
-
+        if(from1UpTo1000 < 500) System.out.println("Eagle");
+        else System.out.println("Tail");
     }
 }
--- src/main/java/school/mjc/stage0/conditions/task2/GreatestNumberPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task2/GreatestNumberPrinter.java
@@ -1,6 +1,14 @@
 package school.mjc.stage0.conditions.task2;
 
 public class GreatestNumberPrinter {
+//    Implement the program that will find the greatest number from two that are passed as parameters to the method and print
+//    the greatest. In case of equality print any of them. Implement program in the code snippet:
     public void printGreatest(int first, int second) {
+        if(first == second || first>second){
+            System.out.println(first);
+        }
+        else{
+            System.out.println(second);
+        }
     }
 }
